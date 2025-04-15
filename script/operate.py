58d08ffd89ddb513fe0435f7f4ac9af95a0fced1
shchan13 <shaohung@usc.edu>
2025-04-15 13:41:14 -0700

    Refactorization

--- script/operate.py
+++ script/operate.py
@@ -23,7 +23,7 @@ def x_operate(row:Series, cfg:Dict):
 #     return inf
 
 def y_operate(row:Series, cfg:Dict):
-    return (row['hl exp'] + 1) / row['hl gen']
+    # return (row['hl exp'] + 1) / row['hl gen']
     # return row['lowerbound']
     # return row['lowerbound'] - row['root lb']
     # return (row['lowerbound'] - row['root lb']) / (row['hl focal'] + row['hl open'] + row['hl cleanup'])
@@ -35,10 +35,7 @@ def y_operate(row:Series, cfg:Dict):
     # return min(row['runtime build HWY'], cfg['time_limit'])
     # return row['runtime build HWY']
     # return min(row['runtime'], cfg['time_limit'])
-    # runtime_hwy = row['runtime build HWY']
-    # if row['solution cost'] > -1 and row['runtime'] + runtime_hwy <= cfg['time_limit']:
-    #     # return row['solution cost'] - row['root cost']
-    #     # return row['runtime']
-    #     return row['solution cost']
-    #     # return (row['lowerbound'] - row['root lb']) / row['hl gen cleanup']
-    # return inf
+    runtime_hwy = row['runtime build HWY']
+    if row['solution cost'] > -1 and row['runtime'] + runtime_hwy <= cfg['time_limit']:
+        return row['runtime'] + runtime_hwy
+    return cfg['time_limit']
